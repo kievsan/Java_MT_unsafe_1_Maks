16ba47c38678e93be0dcae3754a4a779270ef568
kievsan <kievsan@mail.ru>
2025-03-26 02:36:15 +0300

    Initial

--- src/main/java/ru/mail/kievsan/Main_UseAtomicInteger.java
+++ src/main/java/ru/mail/kievsan/Main_UseAtomicInteger.java
@@ -3,34 +3,30 @@ package ru.mail.kievsan;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
 
-public class Main_SynchronizedClassMethod {
-    private static int counter1 = 0;
-    private static int counter2 = 0;
+public class Main_UseAtomicInteger {
+    private static final AtomicInteger counter1 = new AtomicInteger(0);
+    private static final AtomicInteger counter2 = new AtomicInteger(0);
 
     public static void main(String[] args) throws InterruptedException {
         int tasksCount = 100_000;
         CountDownLatch latch = new CountDownLatch(tasksCount);
         ExecutorService executor = Executors.newFixedThreadPool(100);
 
-        for (int i = 0; i < tasksCount; i++) {
-            // safe:
+        for (int i = 0; i < tasksCount; i++)
             executor.submit(() -> {
-                increment(); // synchronized
+                // safe:
+                counter1.addAndGet(1); // atomic
+                counter2.addAndGet(1); // atomic
                 latch.countDown();
             });
-        }
 
         latch.await();
 
-        System.out.println(counter1);
+        System.out.println(counter1); // non-atomic
         System.out.println(counter2);
         System.exit(0);
-    }
-
-    static synchronized void increment() {
-        // unsafe:
-        counter1++; // non-atomic
-        counter2++; // non-atomic
     }
 }
